136f61758438e70a47c44e4b58fbe2e49c9d8799
SuTechs <sumit123210@gmail.com>
2024-11-28 03:12:16 +0530

    screen init

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,5 @@ import 'package:flutter/material.dart';
 const kAppVersion = "1.0.0+1";
 
 const kPurpleSeedColor = Colors.deepPurple;
+
+const kBackgroundColor = Color(0xff1E1E1E);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:rise/screens/habits/habits_screen.dart';
+import 'package:rise/screens/pomodoro/pomodoro_screen.dart';
+import 'package:rise/screens/settings/settings_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const pages = [
+    HabitsScreen(),
+    PomodoroScreen(),
+    SettingsScreen(),
+  ];
+
+  final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: pages,
       ),
-      body: const Center(
-        child: Text('Home Screen'),
+
+      /// Bottom Navigation Bar
+      bottomNavigationBar: _BottomNavigationBar(
+        onTabTapped: (index) {
+          _pageController.jumpToPage(index);
+        },
       ),
     );
   }
 }
 
-// Create main home screen with bottom navigation bar
+/// Bottom Navigation Bar
+class _BottomNavigationBar extends StatefulWidget {
+  const _BottomNavigationBar({required this.onTabTapped});
+
+  final void Function(int) onTabTapped;
+
+  @override
+  State<_BottomNavigationBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<_BottomNavigationBar> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: _currentIndex == 0 ? true : false,
+      onPopInvokedWithResult: (didPop, _) {
+        widget.onTabTapped(0);
+        setState(() {
+          _currentIndex = 0;
+        });
+      },
+      child: BottomNavigationBar(
+        onTap: (index) {
+          widget.onTabTapped(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+
+        /// Bottom Navigation Bar Items
+        items: const [
+          /// Habits
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Habits',
+          ),
+
+          /// Pomodoro
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: 'Pomodoro',
+          ),
+
+          /// Settings
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
