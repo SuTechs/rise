3fb132e557be0acd53838579b99d8dc7694bc49d
SuTechs <sumit123210@gmail.com>
2024-11-29 01:08:58 +0530

    get started screen

--- lib/screens/onboarding/components/components.dart
+++ lib/screens/onboarding/components/components.dart
@@ -1,1 +1,78 @@
-// add you screen wise components in components folders
+import 'package:flutter/material.dart';
+import 'package:rise/theme.dart';
+
+/// Get Started Button - Circular button with arrow icon and border
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key, required this.onTap});
+
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = context.colorScheme;
+
+    final buttonBackgroundColor = colorScheme.onSurface;
+    final buttonForegroundColor = colorScheme.surfaceBright;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: CircleAvatar(
+        backgroundColor: buttonBackgroundColor,
+        radius: 24,
+        child: Container(
+          width: 44,
+          height: 44,
+          decoration: BoxDecoration(
+            color: buttonBackgroundColor,
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(
+              color: buttonForegroundColor,
+              width: 2,
+            ),
+          ),
+          child: Icon(
+            Icons.arrow_forward_ios,
+            color: buttonForegroundColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Get Started Text - Text with gradient color
+class GetStartedText extends StatelessWidget {
+  const GetStartedText({super.key});
+
+  static final Shader _linearGradientPaint = const LinearGradient(
+    colors: <Color>[Color(0xffBF433F), Color(0xffD8AB28)],
+  ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = context.textTheme;
+
+    return Text.rich(
+      style: textTheme.titleLarge?.copyWith(
+        color: const Color(0xffA4A4A4),
+        height: 1.5,
+      ),
+      TextSpan(
+        children: [
+          const TextSpan(text: 'An insanely simple\n'),
+
+          // gradient text (Habit Tracker)
+          TextSpan(
+            text: 'Habit Tracker',
+            style: TextStyle(
+              foreground: Paint()..shader = _linearGradientPaint,
+            ),
+          ),
+
+          const TextSpan(text: ' to track \nyour habits'),
+        ],
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
--- lib/screens/onboarding/get_started_screen.dart
+++ lib/screens/onboarding/get_started_screen.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../data/bloc/app_bloc.dart';
+import '../home.dart';
+import 'components/components.dart';
 
 class GetStartedScreen extends StatelessWidget {
   const GetStartedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final isShowOnboarding =
-        context.select((AppBloc bloc) => bloc.isShowOnboarding);
+    return Scaffold(
+      extendBody: true,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          /// Texts
+          const GetStartedText(),
 
-    if (isShowOnboarding) {
-      return const Placeholder(
-        child: Text('Onboarding Placeholder'),
-      );
-    }
+          /// illustration
+          Image.asset(
+            'assets/yoga_person.png',
+            width: 300,
+            height: 300,
+          ),
 
-    return const Placeholder(
-      child: Text('Get Started (Login) Placeholder'),
+          /// buttons
+          GetStartedButton(
+            onTap: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
